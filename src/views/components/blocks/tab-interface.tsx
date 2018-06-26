ce40fb47153d6f046cd60bcd1e95d587e1ad2822
Lucas Mosele <lucasmosele@gmail.com>
2018-06-26 10:26:02 -0400

    Tabs

--- src/views/components/blocks/tab-interface.tsx
+++ src/views/components/blocks/tab-interface.tsx
@@ -4,43 +4,52 @@ import styled from 'styled-components';
 import { IStyledProps } from '../../../types/theme-types';
 
 import { Route, Switch, Redirect } from 'react-router-dom';
+import * as TabView from './tabs';
+import { Row, Col } from '../layout';
 
 const NavTab = require('react-router-tabs').NavTab;
 
-const Tabs = styled.div`
+const TabsContainer = styled(Row)`
+  margin-top: 30px;
 `;
-
-const AboutTab = ({}) => (
-  <div>
-    about
-  </div>
-);
-const PlanTab = ({}) => (
-  <div>
-    plans
-  </div>
-);
-const ExampleTab = ({}) => (
-  <div>
-    examples
-  </div>
-);
-
+const Tab = styled(NavTab)`
+  display: block;
+  width: 100%;
+  text-align: center;
+  border: 1px solid black;
+  padding: 10px;
+`;
+const TabColumn = styled(Col)`
+  padding: 0;
+`;
+const TabViewMain = styled(Row)`
+  border: 1px solid black;
+`;
+// render={() => <Redirect from='/' to='/about' />}
 export default class TabInterface extends React.Component<any, any> {
   public render() {
     return (
-      <Tabs>
-        <NavTab to='/about'>About</NavTab>
-        <NavTab to='/plan'>Plan</NavTab>
-        <NavTab to='/examples'>Examples</NavTab>
-
-        <Switch>
-          <Route exact={true} path='/' render={() => <Redirect from='/' to='/about' />} />
-          <Route path='/about' component={AboutTab} />
-          <Route path='/plan' component={PlanTab} />
-          <Route path='/examples' component={ExampleTab} />
-        </Switch>
-      </Tabs>
+      <>
+        <TabsContainer between='sm'>
+          <TabColumn sm='4'>
+            <Tab to='/about'>About</Tab>
+          </TabColumn>
+          <TabColumn sm='4'>
+            <Tab to='/plan'>Plan</Tab>
+          </TabColumn>
+          <TabColumn sm='4'>
+            <Tab to='/examples'>Examples</Tab>
+          </TabColumn>
+        </TabsContainer>
+        <TabViewMain>
+          <Switch>
+            <Route exact={true} path='/' />
+            <Route path='/about' component={TabView.AboutTab} />
+            <Route path='/plan' component={TabView.PlanTab} />
+            <Route path='/examples' component={TabView.ExampleTab} />
+          </Switch>
+        </TabViewMain>
+      </>
     );
   }
 }
---
+++ src/views/components/blocks/tabs.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import styled from 'styled-components';
+
+export const AboutTab = ({ }) => (
+  <div>
+    about
+  </div>
+);
+export const PlanTab = ({ }) => (
+  <div>
+    plans
+  </div>
+);
+export const ExampleTab = ({ }) => (
+  <div>
+    examples
+  </div>
+);
