50ef3e697486140e4a27b48145ca48074e555f60
Lucas Mosele <lucasmosele@gmail.com>
2018-06-26 23:33:30 -0400

    Lint

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
+
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
